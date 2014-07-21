3db94c3faf74a8449d677bf523606f4203dec8f3
Jean-Philippe Boily <j@jipi.ca>
2014-07-20 21:58:08 -0400

    add MetaGenerator
    This class is basically getting all required information for meta
    description, title, Facebook Open Graph and Twitter Cards tags.

---
+++ app/services/meta_generator.rb
@@ -0,0 +1,34 @@
+class MetaGenerator
+  include ActionView::Helpers::SanitizeHelper
+  include ActionView::Helpers::TextHelper
+
+  def initialize(post)
+    @post = post
+  end
+
+  def title
+    if for_a_post?
+      "#{@post.title} by @#{Vocal.config.social.twitter.username}"
+    else
+      Vocal.config.meta.title
+    end
+  end
+
+  def description
+    if for_a_post?
+      truncate(strip_tags(@post.html), length: 155)
+    else
+      Vocal.config.meta.description
+    end
+
+  end
+
+  def image
+    Vocal.config.meta.image_url
+  end
+
+  private
+  def for_a_post?
+    !!@post
+  end
+end
---
+++ spec/services/meta_generator_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe MetaGenerator do
+  let(:post) { nil }
+  subject { described_class.new(post) }
+
+  describe '#title' do
+    context 'for a post' do
+      let(:post) { create(:post) }
+
+      it { expect(subject.title).to eq "#{post.title} by @#{Vocal.config.social.twitter.username}" }
+    end
+
+    context 'for main page' do
+      it { expect(subject.title).to eq Vocal.config.meta.title }
+    end
+  end
+
+  describe '#description' do
+    context 'for a post' do
+      let(:post) { create(:post, html: ('<p>this is it!.</p>' * 30) ) }
+
+      it { expect(subject.description).not_to include('<p>')  }
+      it { expect(subject.description.length).to eq 155 }
+    end
+
+    context 'for main page' do
+      it { expect(subject.description).to eq Vocal.config.meta.description }
+    end
+  end
+
+  describe '#image' do
+    it { expect(subject.image).to eq Vocal.config.meta.image_url }
+  end
+end
