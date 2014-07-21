26b42186ff71fb69d7b5ad824ec87864850ef9f2
Jean-Philippe Boily <j@jipi.ca>
2014-07-20 22:40:41 -0400

    add initializer with some of the new config elements

--- spec/dummy/config/initializers/vocal.rb
+++ spec/dummy/config/initializers/vocal.rb
@@ -1,3 +1,9 @@
 Vocal.config.theme = 'default'
 Vocal.config.disqus.shortname = 'vocaldev'
 # Vocal.config.google_analytics.id = 'YOUR Google Analytics ID'
+
+
+Vocal.config.meta.title = 'My Blog'
+Vocal.config.meta.description = 'My awesome blog'
+Vocal.config.meta.image_url = 'http://example.com/image.png'
+Vocal.config.social.twitter.username = 'someone_over_the_rainbom'
