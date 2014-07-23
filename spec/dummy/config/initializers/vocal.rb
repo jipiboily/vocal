1fa0d75c3efdd88e6ea1f0dc4287280215e2b17f
Jean-Philippe Boily <j@jipi.ca>
2014-07-22 23:05:23 -0400

    add config for social links for default theme

--- spec/dummy/config/initializers/vocal.rb
+++ spec/dummy/config/initializers/vocal.rb
@@ -10,3 +10,8 @@ Vocal.config.social.twitter.username = 'someone_over_the_rainbom'
 
 Vocal.config.social.facebook.locale = 'en_US'
 Vocal.config.social.twitter.button.text = 'Tweet'
+
+Vocal.config.theme.default.github_url = 'https://github.com/jipiboily'
+Vocal.config.theme.default.twitter_url = 'https://twitter.com/jipiboily'
+Vocal.config.theme.default.linked_in_url = 'https://www.linkedin.com/in/jipiboily'
+Vocal.config.theme.default.google_plus_url = 'https://plus.google.com/+JeanPhilippeBoily'
